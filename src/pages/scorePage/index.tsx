799a6f8a01a09fe492d531c656b7c1d5396a2548
zkacz00 <zk.zofia.kaczmarczyk@gmail.com>
2024-08-22 10:15:49 +0200

    css changes

--- src/pages/scorePage/index.tsx
+++ src/pages/scorePage/index.tsx
@@ -1,43 +1,70 @@
-import {Link} from 'react-router-dom';
-import components from '../../components';
+import React from "react";
+import { Link } from "react-router-dom";
+import components from "../../components";
+import { type Category } from "../../context/quizUtils";
 
 interface Props {
-   score: number;
-   numberOfQuestions: number;
-   category: string;
+  score: number;
+  numberOfQuestions: number;
+  category: Category;
 }
 const ScorePage = (props: Props) => {
+  const {
+    CategoryButtonsList,
+    CategoryButton,
+    NextButton,
+    Heading,
+    Counter,
+    PageTransition,
+  } = components;
 
-   const {
-      CategoryButtonsList,
-      CategoryButton,
-      NextButton,
-      Heading,
-      Counter,
-   } = components;
-
-   return ( 
+  return (
+    <>
+    <PageTransition>
       <main className="scorePage">
-         <div className="scorePage__mainContent">
-            <div className="scorePage__categoryButton">
-               <CategoryButton category={props.category} location="single"/>
-            </div>
-            <div className="scorePage__score">
-               <Counter location="scorePage" category={props.category} score={props.score} numberOfQuestions={props.numberOfQuestions}/>
-               <Heading type="h1" category={props.category} location="scorePage" text="twój wynik" />
-            </div>
-            <Link to={`/quiz/${props.category}`}>
-               <NextButton category={props.category} text='powtórz quiz' size='big' />
-            </Link>
-         </div>
-         <div className="scorePage__list">
-               <p>wybierz<br />inną kategorię</p> 
-               <div>
-                  <CategoryButtonsList direction="column" location="scorePage" category={props.category}/>
-               </div>
-         </div>
+        <div className="scorePage__mainContent">
+          <div className="scorePage__categoryButton">
+            <CategoryButton category={props.category} location="single" />
+          </div>
+          <div className="scorePage__score">
+            <Counter
+              location="scorePage"
+              category={props.category}
+              score={props.score}
+              numberOfQuestions={props.numberOfQuestions}
+            />
+            <Heading
+              type="h1"
+              category={props.category}
+              location="scorePage"
+              text="twój wynik"
+            />
+          </div>
+          <Link to={`/quiz/${props.category}`}>
+            <NextButton
+              category={props.category}
+              text="powtórz quiz"
+              size="big"
+            />
+          </Link>
+        </div>
+        <div className="scorePage__list">
+          <p>
+            wybierz
+            <br />
+            inną kategorię
+          </p>
+          <CategoryButtonsList
+            direction="column"
+            location="scorePage"
+            category={props.category}
+            visible={true}
+          />
+        </div>
       </main>
-   );
-}
+    </PageTransition>
+    </>
+  );
+};
 
 export default ScorePage;
