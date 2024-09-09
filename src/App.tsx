b20b9ee88c441916b51cbc73fceb27c9e8c0e59b
zkacz00 <zk.zofia.kaczmarczyk@gmail.com>
2024-09-09 13:00:52 +0200

    removed useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './styles/themes/default/theme.scss';
-import React, { useEffect } from 'react';
+import React from 'react';
 import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 
 import MenuPage from './pages/menuPage';
